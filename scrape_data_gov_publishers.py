c54464961a7f6b6528ee6e43b71ab1354ae2e3ff
Michael Spratt <mspratt@Michael-Spratt.com>
2015-04-23 10:53:15 +0200

    TTesting for  elements and CSX files

--- scrape_data_gov_publishers.py
+++ scrape_data_gov_publishers.py
@@ -58,8 +58,8 @@ os.remove("organization_list.json")
 print("********   loop through publishers   ************")
 cntr=0
 for x in organization_data["result"] :
-     #if cntr > 150:  # get only the first 25 publishers
-         #break
+     #if cntr > 8:  # get only the first 25 publishers
+     #    break
 
      publisher_name = organization_data["result"][cntr]
      cntr += 1
@@ -73,196 +73,12 @@ for x in organization_data["result"] :
      #***************************************************
 
 
-     if cntr <= 1307:    #  to skip to a certain record
+     if cntr <= 7:    #  to skip to a certain record
          continue
 
 
       
   
-     #------------------------------------
-     # these publishers have problems with
-     # my code.... they point to a data set,
-     # but once we access there is no data...
-     #
-     #  I thought there were only going to be a few
-     #  if I had know I would have written the
-     #  conditional that checks for this...
-
-     """
-     if publisher_name == "air-accident-investigation-branch":
-          continue   
-     if publisher_name == "animal-health-and-veterinary-laboratories-agency":
-          continue   
-     if publisher_name == "appointments-commission":
-          continue
-     if publisher_name == "architects-registration-board":
-          continue
-     if publisher_name == "british-film-institute":
-          continue
-     if publisher_name == "british-tourist-authority":
-          continue
-     if publisher_name == "businesslink":
-          continue
-     if publisher_name == "centre-for-ecology-hydrology":
-          continue
-     if publisher_name == "centre-for-environment-fisheries-aquaculture-science":
-          continue
-     if publisher_name == "charnwood-borough-council":
-          continue
-     if publisher_name == "chartered-institute-of-public-finance-and-accountancy":
-          continue    
-     if publisher_name == "collections-trust":
-          continue    
-     if publisher_name == "companies-investigation-branch":
-          continue    
-     if publisher_name == "consumer-focus":
-          continue    
-     if publisher_name == "crawley-borough-council":
-          continue    
-     if publisher_name == "crc-energy-efficiency-scheme":
-          continue    
-     if publisher_name == "cycle-streets":
-          continue    
-     if publisher_name == "defence-academy-of-the-united-kingdom":
-          continue    
-     if publisher_name == "defence-support-group":
-          continue    
-     if publisher_name == "department-for-work-and-pensions":
-          continue    
-     if publisher_name == "department-of-justice":
-          continue    
-     if publisher_name == "driver-and-vehicle-licensing-agency":
-          continue    
-     if publisher_name == "driving-standards-agency":
-          continue    
-     if publisher_name == "east-cheshire-nhs-trust":
-          continue    
-     if publisher_name == "education-funding-agency":
-          continue    
-     if publisher_name == "energy-saving-trust":
-          continue    
-     if publisher_name == "fco-services":
-          continue    
-     if publisher_name == "general-register-office-for-scotland":
-          continue    
-     if publisher_name == "general-teaching-council-for-england":
-          continue    
-     if publisher_name == "government-car-and-despatch-agency":
-          continue    
-     if publisher_name == "great-britain-china-centre":
-          continue    
-     if publisher_name == "hm-inspectorate-of-probation":
-          continue    
-     if publisher_name == "hm-passport-office":
-          continue    
-     if publisher_name == "independent-housing-ombudsman":
-          continue  
-     if publisher_name == "independent-living-fund":
-          continue  
-     if publisher_name == "isle-of-wight-council":
-          continue  
-     if publisher_name == "joint-nature-conservation-committee":
-          continue  
-     if publisher_name == "judicial-appointments-commission":
-          continue  
-     if publisher_name == "judicial-office":
-          continue  
-     if publisher_name == "lake-district-national-park":
-          continue
-     if publisher_name == "learning-and-skills-council-skills-funding-agency":
-          continue
-     if publisher_name == "marine-accident-investigation-branch":
-          continue
-     if publisher_name == "maritime-and-coastguard-agency":
-          continue
-     if publisher_name == "marshall-aid-commemoration-commission":
-          continue
-     if publisher_name == "musgrove-park-hospital":
-          continue
-     if publisher_name == "national-college-for-teaching-and-leadership":
-          continue
-     if publisher_name == "national-employment-savings-trust":
-          continue
-     if publisher_name == "national-health-service":
-          continue
-     if publisher_name == "nhs-bedfordshire":
-          continue
-     if publisher_name == "pensions-ombudsman":
-          continue
-     if publisher_name == "nhs-east-cheshire-ccg":
-          continue
-     if publisher_name == "nhs-liverpool-ccg":
-          continue
-     if publisher_name == "oag":
-          continue
-     if publisher_name == "prisons-and-probation-ombudsman":
-          continue
-     if publisher_name == "public-health-england":
-          continue
-     if publisher_name == "remploy-limited":
-          continue
-     if publisher_name == "royal-borough-of-greenwich":
-          continue
-     if publisher_name == "olympic-delivery-authority":
-          continue
-     if publisher_name == "royal-botanic-gardens-kew":
-          continue
-     if publisher_name == "royal-borough-of-greenwich":
-          continue
-     if publisher_name == "rugby-borough-council":
-          continue
-     if publisher_name == "rural-payments:-agency":
-          continue
-     if publisher_name == "rural-payments-agency":
-          continue
-     if publisher_name == "rsecurity-industry-authority":
-          continue
-     if publisher_name == "security-industry-authority":
-          continue
-     if publisher_name == "service-personnel-and-veterans-agency":
-          continue
-
-     if publisher_name == "sport-england":
-          continue
-     if publisher_name == "standards-and-testing-agency":
-          continue
-     if publisher_name == "stockton-on-tees-borough-council":
-          continue
-     if publisher_name == "teignbridge-district-council":
-          continue
-     if publisher_name == "the-food-and-environment-research-agency":
-          continue
-     if publisher_name == "the-northern-lighthouse-board":
-          continue
-     if publisher_name == "the-pensions-regulator":
-          continue
-     if publisher_name == "valuation-office-agency":
-          continue
-     if publisher_name == "vehicle-and-operator-services-agency":
-          continue
-     if publisher_name == "vehicle-certification-agency":
-          continue
-     if publisher_name == "visit-england":
-          continue
-     if publisher_name == "westminster-foundation-for-democracy":
-          continue
-     if publisher_name == "wilton-park-executive-agency":
-          continue
-
-     """
-    
-     if publisher_name == "wolverton-and-greenleys-town-council":
-          continue
-
-     """  foi-web tag missing
-     if publisher_name == "agri-food-and-biosciences-institute":
-          continue   
-     if publisher_name == "belfast-city-council":
-          continue     
-     if publisher_name == "birmingham-childrens-nhs-foundation-trust":
-          continue    
-     """
-
 
      url_1 = "http://data.gov.uk/api/3/action/organization_show?id="+ publisher_name
      http_1 = urllib3.PoolManager()
@@ -339,6 +155,9 @@ for x in organization_data["result"] :
 
          print("publisher_returned = ", publisher_returned)   
          print("publisher_type     = ", publisher_type)
+
+         if len(publisher_data["result"]["packages"][cntr_2]["title"])==0:
+              break
 
          
          #.....................
